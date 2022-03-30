159491636b06a083ceb89798d7b879cdf7737f63
Eriola-al <endoj22@gmail.com>
2022-03-30 17:27:21 +0200

    Commiting

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Background = styled.div`
@@ -40,22 +41,52 @@ const Tag = styled.span.attrs()`
     border: 1px solid grey;
     border-radius: 5px;
     padding: 5px;
+    display: flex;
+    gap: 5px;
 `
 
+
 function App() {
+
+  const [tags, setTags] = useState('');
+  const [changeColor, setChangeColor] = useState(false);
+
+
+  const handleChange = (e) => {
+    setTags(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    // ,.preventDefault();
+    if(e.key === 'Enter') {
+      document.getElementById("text").value = "";
+      setChangeColor(true);
+    }
+  }
+
+  const displayWord = tags.split(/,/);
+
+  if(displayWord.length !== 0) {
+    displayWord.pop()
+  }
+
+    //get random Index Value
+    let randomIndex = displayWord[Math.floor(Math.random() * displayWord.length)];
+
+
+
   return (
     <Background>
       <Title thin={550}>Insert your words and seperate them by comma.
         <SubTitle small='20px'>Press Enter to see the chosen word of the day!</SubTitle>
       </Title>
-      <Textarea rows='5' cols='70' placeholder='Write your words..'>
-      </Textarea>
+      <Textarea rows='5' cols='70' placeholder='Write your words..' onKeyUp={handleKeyPress} id="text" onChange={handleChange}/>
       <Tags>
-        <Tag>myWord</Tag>
-        <Tag>myWord</Tag>
+        {displayWord.map((word, index) => <Tag key={index} style={{backgroundColor: changeColor && ((randomIndex === word) 
+          ? 'blue' : 'orange') }}>
+          {word}</Tag> )} 
       </Tags>
     </Background>
-  );
+  )
 }
-
 export default App;
